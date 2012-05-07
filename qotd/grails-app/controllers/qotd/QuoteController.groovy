c7b685ea4b0e58aabd37f8f569adc52ed81711ac
Jan Kester <jan.c.kester@gmail.com>
2012-05-07 23:12:29 +0200

    Changing datasource

--- qotd/grails-app/controllers/qotd/QuoteController.groovy
+++ qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -2,10 +2,21 @@ package qotd
 
 class QuoteController {
 
-    def index() { }
-	
-	def home = {
-		render "<h1>Real Programmers do not eat Quiche</h1>"
+	def index = { 
+		redirect(action: home) 
 	}
+
+
+	def home = { 
+		render "<h1>Real Programmers do not eat Quiche</h1>" 
+	}
+	
+	def random = {
+		def staticAuthor = "Anonymous"
+		def staticContent = "Real Programmers don't eat much quiche"
+		[ author: staticAuthor, content: staticContent]
+	}
+	
+	def scaffold = Quote
 		
 }
